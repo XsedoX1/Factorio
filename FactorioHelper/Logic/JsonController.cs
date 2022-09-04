1eb7a4e531ea224bcf70302092d3cba669a943dc
Juliusz <julsad3@st.amu.edu.pl>
2022-09-04 15:42:20 +0200

    version 1.0.0.0

--- FactorioHelper/Logic/JsonController.cs
+++ FactorioHelper/Logic/JsonController.cs
@@ -12,15 +12,22 @@ public static class JsonController
     public static void Serializer(Item item)
     {
         var readItem = JsonConvert.SerializeObject(item);
+        if (!Directory.Exists(Constants.PROJECT_DIR + "ItemsDB/"))
+            Directory.CreateDirectory(Constants.PROJECT_DIR + "ItemsDB/");
 
-        if (!File.Exists(Constants.PROJECT_DIR + "Data/ItemsDB/" + item.Id + ".json"))
-            File.WriteAllText(Constants.PROJECT_DIR + "Data/ItemsDB/" + item.Id + ".json", readItem);
+        if (!File.Exists(Constants.PROJECT_DIR + "ItemsDB/" + item.Id + ".json"))
+            File.WriteAllText(Constants.PROJECT_DIR + "ItemsDB/" + item.Id + ".json", readItem);
     }
 
     public static ObservableCollection<Item> Deserializer()
     {
         var listOfItems = new ObservableCollection<Item>();
-        var files = Directory.GetFiles(Constants.PROJECT_DIR + "Data/ItemsDB/");
+
+        if (!Directory.Exists(Constants.PROJECT_DIR + "ItemsDB/"))
+            Directory.CreateDirectory(Constants.PROJECT_DIR + "ItemsDB/");
+
+        var files = Directory.GetFiles(Constants.PROJECT_DIR + "ItemsDB/");
+
         foreach (var path in files)
         {
             var serializedItem = File.ReadAllText(path);
